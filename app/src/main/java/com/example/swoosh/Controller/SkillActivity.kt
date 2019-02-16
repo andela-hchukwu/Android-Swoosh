c5c183c4d40bc5517bc178bb6b2de25330e5720e
Henry Chukwu <henry.chukwu@andela.com>
2019-02-16 03:20:04 +0100

    app icon

--- app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/SkillActivity.kt
@@ -19,6 +19,18 @@ class SkillActivity : BaseActivity() {
         player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     fun onBallerClicked(view: View) {
         beginnerSkillBtn.isChecked = false
         player.skill = "Baller"
